f78d608707234eef8ee0a165b1ce723419c80826
walaeddine01 <walaaeddine33@gmail.com>
2024-04-26 19:07:55 +0200

    I made the changes on 2024-04-26 19:07:55

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- contains id and name and email
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
