ceb030e472f40510b68ba130b612663b21b0d753
walaeddine01 <walaaeddine33@gmail.com>
2024-04-26 19:40:22 +0200

    I made the changes on 2024-04-26 19:40:22

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- this script will create a table called users 
+-- that contains id, name, email, and country columns
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
