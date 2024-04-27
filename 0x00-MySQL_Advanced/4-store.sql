c44e75d01f15c5fdb9e327abda192ba55d5c6f1d
walaeddine01 <walaaeddine33@gmail.com>
2024-04-27 21:27:12 +0200

    I made the changes on 2024-04-27 21:27:12

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,9 @@
 --
 --
-CREATE TRIGGER update_item_quantity AFTER INSERT ON orders
+CREATE TRIGGER IF NOT EXISTS update_item_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
+    SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
 END;
