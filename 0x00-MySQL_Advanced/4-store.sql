2156d6d71758e79663300567729b121c25d6e5eb
walaeddine01 <walaaeddine33@gmail.com>
2024-04-27 21:43:22 +0200

    I made the changes on 2024-04-27 21:43:22

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,9 @@
 -- this script creates a trigger that decreases
 -- the quantity of an item after adding a new order.
+DROP TRIGGER IF EXISTS update_item_quantity;
 DELIMITER //
-CREATE TRIGGER IF NOT EXISTS update_item_quantity AFTER INSERT ON orders
+CREATE TRIGGER update_item_quantity
+AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
