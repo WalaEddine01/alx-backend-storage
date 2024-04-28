c14cfde95c518971acff4f6d8fedc50c8489237d
walaeddine01 <walaaeddine33@gmail.com>
2024-04-28 14:31:04 +0200

    I made the changes on 2024-04-28 14:31:04

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,16 +2,17 @@
 -- The procedure first checks if the project exists in the projects table.
 -- If it does not exist, it creates a new project and retrieves its id.
 DELIMITER //
-CREATE PROCEDURE IF NOT EXISTS addBonus (user_id INT, project_name VARCHAR(255), score INT)
+DROP PROCEDURE IF EXISTS AddBonus;
+CREATE PROCEDURE addBonus (user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
     DECLARE project_id INT;
 
-    SELECT id FROM projects WHERE name = project_name;
+    SELECT id FROM projects WHERE name = project_name INTO project_id;
     IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
+        SELECT LAST_INSERT_ID() INTO project_id;
     END IF;
-
+    SELECT id FROM projects WHERE name = project_name INTO project_id;
     INSERT INTO corrections(user_id, project_id, score) VALUES (user_id, project_id, score);
 END //
 DELIMITER ;
